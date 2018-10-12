7084bce7f3f3067ffa7d47e7dccecf112c41d796
wsrcdr <rcdrzain@gmail.com>
2018-10-12 15:39:55 +0100

    commit

---
+++ MvcMovies/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovies.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovies.Models.Movie> Movie { get; set; }
+    }
+}
